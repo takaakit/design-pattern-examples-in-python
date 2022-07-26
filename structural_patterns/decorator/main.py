7649ca98acf1591a0e225e6c80473eeaadfba592
takaakit <takaaki.teshima.dev@gmail.com>
2022-07-26 14:08:55 +0900

    refactoring

--- structural_patterns/decorator/main.py
+++ structural_patterns/decorator/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from structural_patterns.decorator.display import Display
 from structural_patterns.decorator.message_display import MessageDisplay
 from structural_patterns.decorator.side_frame import SideFrame
 from structural_patterns.decorator.full_frame import FullFrame
@@ -9,11 +10,11 @@ Display a string with decorative frames. The frames can be combined arbitrarily.
 '''
 
 if __name__ == '__main__':
-    display_a = MessageDisplay('Nice to meet you.')
+    display_a: Display = MessageDisplay('Nice to meet you.')
     display_a.show()
 
-    display_b = SideFrame(MessageDisplay('Nice to meet you.'), '!')
+    display_b: Display = SideFrame(MessageDisplay('Nice to meet you.'), '!')
     display_b.show()
 
-    display_c = FullFrame(SideFrame(MessageDisplay('Nice to meet you.'), '!'))
+    display_c: Display = FullFrame(SideFrame(MessageDisplay('Nice to meet you.'), '!'))
     display_c.show()
